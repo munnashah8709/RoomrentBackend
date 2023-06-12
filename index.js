1d4f5b25288f4dbe83ff72ba794afe0d4bec11ae
munna shah <munnashah8709@gmail.com>
2023-06-12 10:04:14 +0530

    login

--- index.js
+++ index.js
@@ -56,6 +56,34 @@ app.post('/register', async (req, res) => {
 });
 
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Check if the user exists
+      const user = await userregistration.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+  
+      // Compare the provided password with the stored hashed password
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+  
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id }, secret);
+  
+      // Return the token as a response
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ message: 'Login failed' });
+    }
+  });
+
+  
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
